e3ecb18d030984a73d9b0f0de904a72c97830641
PrajaktaBade <prajakta10909@gmail.com>
2025-09-09 11:40:06 +0100

    Remove unrelated folders outside repo

--- AWS project/cafe-sales-analytics-aws/phase2-lambda-s3-trigger/lambda_function.py
+++
@@ -1,8 +0,0 @@
-import json
-
-def lambda_handler(event, context):
-    print("Event:", json.dumps(event))
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
