46ea78afa486f439ae5569310a69802c29630196
PrajaktaBade <prajakta10909@gmail.com>
2025-09-09 11:11:13 +0100

    Remove accidentally added folders from repo

--- Daily Python Projects/Beginner Projects /rest_test.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = "https://jsonplaceholder.typicode.com/users/1"  # test API
-response = requests.get(url)
-
-if response.status_code == 200:
-    data = response.json()
-    print(data)
